3af0156ee3ef7f454e407df97a1909da5c9b7951
Zhannur <221952@astanait.edu.kz>
2023-05-01 20:18:52 +0600

    Approval of methods

---
+++ src/MyList.java
@@ -0,0 +1,16 @@
+public interface MyList<T> {
+    int size();
+    boolean contains(Object o);
+    void add(T item);
+    void add(T item, int index);
+    boolean remove(T item);
+    T remove(int index);
+    void clear();
+    T get(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    void sort();
+
+    void addAll(MyList O);
+}
+
