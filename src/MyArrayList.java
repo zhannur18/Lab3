a24be711893b24cfd8ba0c685cf7486cd7eca1c3
Zhannur <221952@astanait.edu.kz>
2023-05-03 01:11:27 +0600

    array list class

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,22 +1,22 @@
 import java.util.Arrays;
-public class MyArrayList  <T extends Comparable<T>> implements MyList{
+public abstract class MyArrayList  <T extends Comparable<T>> implements MyList {
     int size = 0;
     int[] arr;
 
-    public MyArrayList(){
-        arr  = new int[5];
+    public MyArrayList() {
+        arr = new int[5];
     }
 
 
     @Override
-    public int size() {
+    public int size() { //Returns the number of elements in the array
         return size;
     }
 
     @Override
     public boolean contains(Object o) {//It determines if a specific item is present in the arrayList.
-        for(int i=0; i<size; i++){
-            if (arr[i] == (int)o){
+        for (int i = 0; i < size; i++) {//Returns  if the array contains the specified element
+            if (arr[i] == (int) o) {
                 return true;
             }
 
@@ -26,34 +26,34 @@ public class MyArrayList  <T extends Comparable<T>> implements MyList{
 
     @Override
     public void add(Object item) {// adding a particular item to an index
-        arr[size]= (int)item;
-        size =size + 1;
+        arr[size] = (int) item;//Adds the specified element  to the end of the array
+        size = size + 1;
     }
 
     @Override
-    public void add(Object item, int index) {
-        if(arr.length == size){
+    public void add(Object item, int index) {//Inserts the specified element  at the specified  in the array
+        if (arr.length == size) {
             increasebuffer();
         }
-        for(int i=size-1; i >=index; i--){
-            arr[i+1]= arr[i];
+        for (int i = size - 1; i >= index; i--) {
+            arr[i + 1] = arr[i];
         }
-        arr[index]= (int)item;
+        arr[index] = (int) item;
         size++;
     }
 
-    public void increasebuffer(){// It is a function that increases
-        int[] temp = new int[size + 5];
-        for(int i=0; i<size; i++){
+    public void increasebuffer() {// It is a function that increases
+        int[] temp = new int[size + 5];// Increases the size of the array by creating a new array with 5 more elements and copying the old elements into it
+        for (int i = 0; i < size; i++) {
             temp[i] = arr[i];
         }
-        arr=temp;
+        arr = temp;
     }
 
     @Override
-    public boolean remove(Object item) {
-        for(int i=0;i<size;i++){
-            if ((int)item== arr[i]) {
+    public boolean remove(Object item) {//Removes the first occurrence of the specified element  from the array, if it exists.
+        for (int i = 0; i < size; i++) {
+            if ((int) item == arr[i]) {
                 remove(i);
                 size--;
                 return true;
@@ -65,71 +65,22 @@ public class MyArrayList  <T extends Comparable<T>> implements MyList{
 
     @Override
     public Object remove(int index) {// this procedure moves each element by one, overwriting the element to be deleted.
-        int temp = arr[index];
-        for(int i=index; i <size-1; i++){
-            arr[i]= arr[i+1];
+        int temp = arr[index];//Removes the element at the specified  from the array, shifting the elements after  to the left by one position
+        for (int i = index; i < size - 1; i++) {
+            arr[i] = arr[i + 1];
         }
-        arr[size-1] = 0;
+        arr[size - 1] = 0;
         size--;
         return temp;
     }
 
-    @Override
-    public void clear() {
-        Arrays.fill(arr,0);
-        size =0;
-    }
-
-    @Override
-    public Object get(int index) {
-        return arr[index];
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        for (int i=0;i>size; i++){
-            if (arr[i]==(int)o){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        for(int i= size -1; i>=0; i--){
-            if (arr[i]==(int)o){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    @Override
-    public void sort() {
-        Arrays.sort(arr,0,size);
-    }
-
-    public void print() {
-        for(int i=0; i< size; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-    public void addAll(MyList o) {
-        for(int i = 0; i < o.size(); i++){
-            add(o.get(i));
-        }
-
 }
-    public int addLast(Object item){ //add last
-        if(arr.length <= size){
-            increasebuffer();
-            arr[size] = (int) item;
-        } else {
-            arr[size] = (int) item;
-        }
-        int temp = arr[size];
-        size++;
-        return temp;
-    }
-}
+
+
+    //boolean empty() – Returns whether the stack is empty
+
+        //int size() – Returns the size of the stack
+
+        //peek() – Returns a reference to the topmost element of the stack
+        //push(T) – Adds the element at the top of the stack
+        //pop() – Retrieves and deletes the topmost element of the stack
